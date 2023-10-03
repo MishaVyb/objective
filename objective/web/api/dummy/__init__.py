a7fdcf2d80900d63845fec699f83e8de98d012ba
Misha Vybornyi <vbrn.mv@gmail.com>
2023-10-03 18:58:32 +0400

    initial

---
+++ objective/web/api/dummy/__init__.py
@@ -0,0 +1,4 @@
+"""Dummy model API."""
+from objective.web.api.dummy.views import router
+
+__all__ = ["router"]
---
+++ objective/web/api/monitoring/__init__.py
@@ -0,0 +1,4 @@
+"""API for checking project status."""
+from objective.web.api.monitoring.views import router
+
+__all__ = ["router"]
