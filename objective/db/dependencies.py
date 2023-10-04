5ad4ddc656a531717c9d69f71995c0c5c64c4d40
Misha Vybornyi <vbrn.mv@gmail.com>
2023-10-04 21:46:34 +0400

    fix dep

--- objective/db/dependencies.py
+++ objective/db/dependencies.py
@@ -1,12 +1,10 @@
 from typing import AsyncGenerator
 
-from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, AsyncSessionTransaction
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
 from starlette.requests import Request
 
 
-async def get_db_session(
-    request: Request,
-) -> AsyncGenerator[AsyncSessionTransaction, None]:
+async def get_db_session(request: Request) -> AsyncGenerator[AsyncSession, None]:
     """
     Create and get database session.
 
@@ -15,5 +13,5 @@ async def get_db_session(
     """
     engine: AsyncEngine = request.app.state.db_engine
     async with AsyncSession(engine, expire_on_commit=False) as session:
-        async with session.begin() as session_transaction:
-            yield session_transaction
+        async with session.begin():
+            yield session
