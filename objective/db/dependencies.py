bec4f18c68ed4c38f741fdc7d60df6f61f455a50
Misha Vybornyi <vbrn.mv@gmail.com>
2023-10-04 16:31:41 +0400

    fix session dep - session.begin

--- objective/db/dependencies.py
+++ objective/db/dependencies.py
@@ -1,20 +1,19 @@
 from typing import AsyncGenerator
 
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, AsyncSessionTransaction
 from starlette.requests import Request
 
 
-async def get_db_session(request: Request) -> AsyncGenerator[AsyncSession, None]:
+async def get_db_session(
+    request: Request,
+) -> AsyncGenerator[AsyncSessionTransaction, None]:
     """
     Create and get database session.
 
     :param request: current request.
     :yield: database session.
     """
-    session: AsyncSession = request.app.state.db_session_factory()
-
-    try:  # noqa: WPS501
-        yield session
-    finally:
-        await session.commit()
-        await session.close()
+    engine: AsyncEngine = request.app.state.db_engine
+    async with AsyncSession(engine, expire_on_commit=False) as session:
+        async with session.begin() as session_transaction:
+            yield session_transaction
--- objective/web/lifetime.py
+++ objective/web/lifetime.py
@@ -1,7 +1,7 @@
 from typing import Awaitable, Callable
 
 from fastapi import FastAPI
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine
 
 from objective.settings import settings
 
@@ -17,12 +17,7 @@ def _setup_db(app: FastAPI) -> None:  # pragma: no cover
     :param app: fastAPI application.
     """
     engine = create_async_engine(str(settings.db_url), echo=settings.db_echo)
-    session_factory = async_sessionmaker(
-        engine,
-        expire_on_commit=False,
-    )
     app.state.db_engine = engine
-    app.state.db_session_factory = session_factory
 
 
 def register_startup_event(
@@ -43,7 +38,6 @@ def register_startup_event(
         app.middleware_stack = None
         _setup_db(app)
         app.middleware_stack = app.build_middleware_stack()
-        pass  # noqa: WPS420
 
     return _startup
 
@@ -61,7 +55,5 @@ def register_shutdown_event(
     @app.on_event("shutdown")
     async def _shutdown() -> None:  # noqa: WPS430
         await app.state.db_engine.dispose()
-
-        pass  # noqa: WPS420
 
     return _shutdown
